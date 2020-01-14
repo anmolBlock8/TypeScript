18955bbd2d9407a7e9e8d21f81287d3cdbfe3f86
anmolBlock8 <59727996+anmolBlock8@users.noreply.github.com>
2020-01-14 11:16:27 +0530

    Add files via upload
    BasicTypes

---
+++ app.js
@@ -0,0 +1,10 @@
+var person = {
+    name: 'Anmol',
+    age: 21,
+    tags: ['a', 'b']
+};
+for (var _i = 0, _a = person.tags; _i < _a.length; _i++) {
+    var i = _a[_i];
+    console.log(i);
+}
+// console.log(person) 
---
+++ app.ts
@@ -0,0 +1,10 @@
+const person  = {
+    name: 'Anmol',
+    age: 21,
+    tags: ['a', 'b']
+};
+
+for(const i of person.tags) {
+    console.log(i);
+}
+// console.log(person) 
